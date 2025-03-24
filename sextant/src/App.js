98462d9cf99d6825606c5bc09ac03da5c6e4a2e6
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-03-24 09:02:51 -0400

    Try to clean up Latency code and fix errors.

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -3,25 +3,25 @@ import './App.css';
 import Banner from './Banner';
 import Exhibit from './Exhibit';
 import AddressDisplay from './IPAddress';
-import Latency from './components/Latency';
+import PylonConnector from './PylonConnector';
 
 class App extends Component {
-  render() {
-      return (
-          <div className="App">
-              <Banner bannerText="Sextant" />
-              <Exhibit name="Public IPv4 Address">
-                  <AddressDisplay url='https://api.ipify.org?format=json' />
-              </Exhibit>
-              <Exhibit name="Public IPv6 Address">
-                  <AddressDisplay url='https://api64.ipify.org?format=json' />
-              </Exhibit>
-              <Exhibit heading="Network Performance">
-              <Latency />
-            </Exhibit>
-          </div>
-      );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Banner bannerText="Sextant" />
+                <Exhibit name="Public IPv4 Address">
+                    <AddressDisplay url='https://api.ipify.org?format=json' />
+                </Exhibit>
+                <Exhibit name="Public IPv6 Address">
+                    <AddressDisplay url='https://api64.ipify.org?format=json' />
+                </Exhibit>
+                <Exhibit name="Pylon Packet Latency">
+                    <PylonConnector />
+                </Exhibit>
+            </div>
+        );
+    }
 }
 
 export default App;
